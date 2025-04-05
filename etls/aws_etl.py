cdd162aa6f53058878dec3d3541ef56826407a4e
Ajay1812 <a.kumar01c@gmail.com>
2025-04-05 11:30:41 +0530

    upload csv to s3

--- etls/aws_etl.py
+++ etls/aws_etl.py
@@ -9,25 +9,26 @@ def connect_to_s3():
                 aws_secret_access_key=AWS_ACCESS_KEY,
                 region_name=AWS_REGION
             )
-       print('connected to s3')
+       print('✅ connected to s3')
        return s3
     except Exception as e:
-        print("ERROR: ", e)
+        print("❌ ERROR: ", e)
 
 
 def create_bucket_if_not_exist(s3,bucket):
     try:
         if not boto3.resource('s3'):
             s3.create_bucket(Bucket=bucket, createBucketConfiguration={'LocationConstraint': AWS_REGION})
-            print("Bucket is created")
+            print("✅ Bucket is created")
         else:   
-            print("Bucket already exists")
+            print("❗Bucket already exists")
     except Exception as e:
-        print(e)        
+        print("❌ ERROR: ", e)    
 
 def upload_to_s3(s3 : boto3.client, file_path: str, bucket: str, s3_fle_name:str):
     try:
-        s3.upload_file(file_path, bucket+'/raw/' , s3_fle_name)
+        s3.put_object(Bucket=bucket, Key='raw/')
+        s3.upload_file(file_path, bucket , 'raw/'+s3_fle_name)
         print(f"✅ Uploaded '{file_path}' to bucket '{bucket}' as '{s3_fle_name}'")
     except Exception as e:
         print(f"❌ Upload failed: {e}")
